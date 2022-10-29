285673cae74e1b8aad27a42bcfc92d0356dba68b
Alina <90398251+aloncess@users.noreply.github.com>
2022-10-29 03:39:32 +0300

    Setup opening a web page in a browser window

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+/*function that creates the browser window and loads index.html into a new BrowserWindow instance*/
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+
+    win.loadFile('index.html')
+}
+
+/*Calling the createWindow() function to open the window*/
+app.whenReady().then(() => {
+    createWindow()
+
+    /*Listening for the app module's activate event, and calling the existing createWindow() method if no browser windows are open.*/
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
