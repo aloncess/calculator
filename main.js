0fdee0478a4295502689bab7d3027be2cd7ea2f0
Alina <90398251+aloncess@users.noreply.github.com>
2022-10-30 04:26:44 +0300

    Connect preload.js with main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 /*function that creates the browser window and loads index.html into a new BrowserWindow instance*/
 function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
